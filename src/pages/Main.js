042f91af133af21e095fe6e115c4f333a15bf849
Isaque Akamine <isaqueakamine@gmail.com>
2020-02-04 11:53:54 -0300

    add devs to main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,90 @@
 import React from 'react';
 
-export default function Main() {
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
+export default function Main({ match }) {
     return (
-        <h1>Hello World</h1>
+        <div className="main-container">
+            <img src={logo} alt="Tindev" />
+            <ul>
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/8368427?v=4" alt="" />
+                    <footer className="">
+                        <strong>Isaque Akamine</strong>
+                        <p>Desenvolvedor mobile especializado em React Native</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+                    </div>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/8368427?v=4" alt="" />
+                    <footer className="">
+                        <strong>Isaque Akamine</strong>
+                        <p>Desenvolvedor mobile especializado em React Native</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+                    </div>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/8368427?v=4" alt="" />
+                    <footer className="">
+                        <strong>Isaque Akamine</strong>
+                        <p>Desenvolvedor mobile especializado em React Native</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+                    </div>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/8368427?v=4" alt="" />
+                    <footer className="">
+                        <strong>Isaque Akamine</strong>
+                        <p>Desenvolvedor mobile especializado em React Native</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+                    </div>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
     );
 }
